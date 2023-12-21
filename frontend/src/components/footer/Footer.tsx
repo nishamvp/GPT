55afe80f364802c3535d43881e862cf99ef7daf7
nishamvp <107843151+nishamvp@users.noreply.github.com>
2023-12-21 21:41:47 +0400

    Home page is created and completed, this is the last commit ,this is the final product

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+  return (
+    <footer>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <p style={{fontSize:"18px"}}>Build by nish<span style={{color:"#64f3d5"}}>@</span>m</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import TypingAnim from "../components/typer/TypingAnim";
+import Footer from "../components/footer/footer";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <Box sx={{ width: "100%" }}>
+      <Box
+        sx={{
+          justifyContent: "center",
+          display: "flex",
+        }}
+      >
+        <TypingAnim />
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          my: 10,
+          gap: 5,
+          flexDirection: { md: "row", sm: "column", xs: "column" },
+        }}
+      >
+        <img
+          style={{ width: "20%", margin: "auto" }}
+          src="robot.png"
+          alt="robot"
+        />
+        <img
+          className="image-inverted rotate"
+          style={{ width: "15%", margin: "auto" }}
+          src="openai.png"
+          alt="robot"
+        />
+      </Box>
+      <Box sx={{ display: "flex", width: "100%", mx: "auto" }}>
+        <img
+          style={{
+            display: "flex",
+            margin: "auto",
+            width: "50%",
+            boxShadow: "-5px -5px 105px #64f3d5",
+            borderRadius: 20,
+            marginTop: 20,
+            marginBottom: 20,
+          }}
+          src="chat.png"
+          alt="chat"
+        />
+      </Box>
+      <Footer/>
+    </Box>
+  );
+};
 
-export default HomePage
+export default HomePage;
